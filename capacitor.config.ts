e565c7c47a1ed856316dd265e581289b4bddfbce
roos198686 <elieimmobilier468@gmail.com>
2026-09-19 16:03:50 +0800

    fix(ios): safe-area/nav/camera/tabbar (capacitor.config.ts)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'www.stockings.top',
   appName: 'SKS',
-  webDir: 'dist'
+  webDir: 'dist',
+  // iOS 開啟左邊緣右滑返回上一頁
+  ios: {
+    allowsBackForwardNavigationGestures: true,
+  },
 };
 
 export default config;
